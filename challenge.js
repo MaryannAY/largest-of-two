6e2a0c5253e96a73cb78f9a8c7afc297bfdb81de
MaryannAY <maryann.ahmed2@gmail.com>
2023-07-27 00:36:02 -0700

    js task

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+let a=50
+let b=60
+
+if (a>b){
+    console.log("The largest number is 50" )
+}
+else if (b>a){
+    console.log("The largest number out of the Two numbers is 60")
+}
+
